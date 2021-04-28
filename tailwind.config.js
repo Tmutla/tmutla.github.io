e5a9b9df5f8ab2c17b8cc14ced819e01f16f0901
Joel Corey <joelcorey@fastmail.com>
2021-04-28 08:11:47 -0700

    test text background, bg-img, adjust

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,13 +14,30 @@ module.exports = {
 				light: '#fcfcfc',
 				DEFAULT: '#f2f2f2',
 				dark: '#efefef'
+			},
+			black: {
+				dark: '#000000',
+				DEFAULT: '#080808',
+				light: '#101010'
+			},
+			darkgray: {
+				DEFAULT: '#181818',
+			},	
+			mediumgray: {
+				DEFAULT: '#404040',
+			},
+			lightgray: {
+				DEFAULT: '#b3b3b3',
 			}
     },
     extend: {
 			backgroundImage: theme => ({
-			 'hero1': "url('/img/pexels-photo-1054397.jpeg')", 
-			 'hero2': "url('/img/pexels-photo-5474284.jpeg')",
-			 'hero3': "url('/img/pexels-photo-5483071.jpeg')",
+			 'hero-soldier-computer-1': "url('/img/hero/soldier-computer-001-720x405.jpg')", 
+			 'hero-soldier-computer-2': "url('/img/hero/soldier-computer-001-960x540.jpg')", 
+			 'hero-soldier-computer-3': "url('/img/hero/soldier-computer-001-1280x720.jpg')", 
+			 'hero-soldier-computer-4': "url('/img/hero/soldier-computer-001-1920x1080.jpg')",
+			 'hero-soldier-computer-5': "url('/img/hero/soldier-computer-001-2560x1440.jpg')",
+			 'hero-soldier-computer-6': "url('/img/hero/soldier-computer-001-3840x2160.jpg')", 
 			})
 		}
   },
