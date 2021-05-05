c6cf8da4b8674949379e73ea718334cb0eeed352
Joel Corey <joelcorey@fastmail.com>
2021-05-05 12:57:37 -0700

    testing desktop nav snap

--- js/scripts.js
+++ js/scripts.js
@@ -14,3 +14,21 @@ function slideout() {
 		}
 	}
 }
+
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+
+
+window.onload = function () {
+	window.onscroll = function() {scrollFunction()};
+
+	let header = document.getElementById("header")
+
+	function scrollFunction() {
+		// console.log('hi')
+		if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+			header.classList.add("header-snap");
+		} else {
+			header.classList.remove("header-snap");
+		}
+	} 
+}
