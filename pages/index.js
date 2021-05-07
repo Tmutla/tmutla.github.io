ec46799d30e0deafd1283dcb53188ff15ca0f036
Joel Corey <joelcorey@fastmail.com>
2021-05-07 05:31:21 -0700

    dir change

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '/styles/Home.module.css'
 
-import HeroBlockIntro from '../component/HeroBlockIntro/HeroBlockInro'
-import Footer from '../component/Footer/Footer'
+import HeroBlockIntro from '/component/HeroBlockIntro/HeroBlockInro'
+import Footer from '/component/Footer/Footer'
 
 export default function Home() {
   return (
