e6a196cc35364edd45caa76c5070fd792f1b1912
Joel Corey <joelcorey@fastmail.com>
2021-05-11 13:11:08 -0700

    better page styling for blog main

--- pages/blog.js
+++ pages/blog.js
@@ -1,24 +1,53 @@
+import Head from 'next/head'
 import Link from 'next/link'
 import groq from 'groq'
 import client from '../client'
 
+import Header from '/component/Header/Header'
+import HeroTitle from '/component/HeroTitle/HeroTitle'
+import Footer from '/component/Footer/Footer'
+
 export default function Blog (props) {
     const { posts = [] } = props
     return (
-      <div>
-        <h1>Welcome to a blog!</h1>
-        {posts.map(
-          ({ _id, title = '', slug = '', _updatedAt = '' }) =>
-            slug && (
-              <li key={_id}>
-                <Link href="/post/[slug]" as={`/post/${slug.current}`}>
-                  <a>{title}</a>
-                </Link>{' '}
-                ({new Date(_updatedAt).toDateString()})
-              </li>
-            )
-        )}
-      </div>
+      
+			<div className="">
+				<Head>
+					<title>Tmutla</title>
+					<meta name="description" content="Tmutla Security" />
+					<link rel="icon" href="/favicon.ico" />
+				</Head>
+
+				<main className="">
+				
+					<Header />
+
+					<HeroTitle
+						title="Blog"
+					/>
+
+					<section class="text-gray-600 body-font">
+						<div class=" px-5 mx-auto flex flex-col">
+							{posts.map(
+								({ _id, title = '', slug = '', _updatedAt = '' }) =>
+									slug && (
+										<li key={_id}>
+											<Link href="/post/[slug]" as={`/post/${slug.current}`}>
+												<a>{title}</a>
+											</Link>{' '}
+											({new Date(_updatedAt).toDateString()})
+										</li>
+									)
+							)}
+						</div>
+					</section>
+
+					
+      	</main>
+
+				<Footer />
+
+			</div>
     )
 }
 
