50cdbb9983cf43e64fe2016b32c8b3e7f0099934
Joel Corey <joelcorey@fastmail.com>
2021-05-11 14:43:25 -0700

    wip

--- pages/blog.js
+++ pages/blog.js
@@ -9,6 +9,7 @@ import Footer from '/component/Footer/Footer'
 
 export default function Blog (props) {
     const { posts = [] } = props
+		console.log(posts)
     return (
       
 			<div className="">
@@ -26,19 +27,39 @@ export default function Blog (props) {
 						title="Blog"
 					/>
 
-					<section class="text-gray-600 body-font">
-						<div class=" px-5 mx-auto flex flex-col">
-							{posts.map(
-								({ _id, title = '', slug = '', _updatedAt = '' }) =>
-									slug && (
-										<li key={_id}>
-											<Link href="/post/[slug]" as={`/post/${slug.current}`}>
-												<a>{title}</a>
-											</Link>{' '}
-											({new Date(_updatedAt).toDateString()})
-										</li>
-									)
-							)}
+					<section class="text-gray-600 body-font overflow-hidden">
+						<div class="container px-5 py-24 mx-auto">
+							<div class="-my-8 divide-y-2 divide-gray-100">
+
+								{posts.map(
+									({ _id, title = '', description = '', categories = '', slug = '', _updatedAt = '' }) =>
+										slug && (
+
+											<div class="py-8 flex flex-wrap md:flex-nowrap">
+												<div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+													{/* <span class="font-semibold title-font text-gray-700">Categories</span> */}
+													<span class="mt-1 text-gray-500 text-sm">{new Date(_updatedAt).toDateString()}</span>
+												</div>
+												<div class="md:flex-grow">
+													<h2 class="text-2xl font-medium text-gray-900 title-font mb-2">{title}</h2>
+													<p class="leading-relaxed">description goes here</p>
+													
+													<a className="text-indigo-500 inline-flex mt-4">
+														<Link href="/post/[slug]" as={`/post/${slug.current}`}>
+															Learn More
+														</Link>{' '}
+														<svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2" fill="none" stroke-linecap="round" stroke-linejoin="round">
+															<path d="M5 12h14"></path>
+															<path d="M12 5l7 7-7 7"></path>
+														</svg>
+													</a>
+													
+												</div>
+											</div>
+											
+										)
+								)}
+							</div>
 						</div>
 					</section>
 
