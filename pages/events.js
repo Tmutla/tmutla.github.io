ff4d655a90dd6c2b2993cd6d3c051fc63aef7b58
Joel Corey <joelcorey@fastmail.com>
2021-05-13 12:25:53 -0700

    events

---
+++ pages/events.js
@@ -0,0 +1,40 @@
+import Link from 'next/link'
+import Head from 'next/head'
+
+import Header from '/component/Header/Header'
+import HeroTitle from '/component/HeroTitle/HeroTitle'
+import Footer from '/component/Footer/Footer'
+
+export default function Page() {
+  return (
+    <div className="">
+      <Head>
+        <title>Tmutla</title>
+        <meta name="description" content="Tmutla Security" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="">
+			
+				<Header />
+
+				<HeroTitle
+					title="Events"
+				/>
+				
+				<section className="text-gray-600 body-font">
+					<div className=" px-5 mx-auto flex flex-col">
+					
+					<p className="mt-10">Events coming soon. Stay tuned!</p>
+					
+					</div>
+				</section>
+			        
+      </main>
+
+			<Footer />
+
+    </div>
+  )
+}
+
