7285380ed744b133035983c15f077ae7c6baece1
Joel Corey <joelcorey@fastmail.com>
2021-05-13 12:22:33 -0700

    add back blog

--- pages/blog.js
+++ pages/blog.js
@@ -27,28 +27,28 @@ export default function Blog (props) {
 						title="Blog"
 					/>
 
-					<section class="text-gray-600 body-font overflow-hidden">
-						<div class="container px-5 py-24 mx-auto">
-							<div class="-my-8 divide-y-2 divide-gray-100">
+					<section className="text-gray-600 body-font overflow-hidden">
+						<div className="container px-5 py-24 mx-auto">
+							<div className="-my-8 divide-y-2 divide-gray-100">
 
 								{posts.map(
 									({ _id, title = '', description = '', categories = '', slug = '', _updatedAt = '' }) =>
 										slug && (
 
-											<div class="py-8 flex flex-wrap md:flex-nowrap">
-												<div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
-													<span class="font-semibold title-font text-gray-700">{categories[0].title}</span>
-													<span class="mt-1 text-gray-600 text-sm">{new Date(_updatedAt).toDateString()}</span>
+											<div className="py-8 flex flex-wrap md:flex-nowrap">
+												<div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
+													<span className="font-semibold title-font text-gray-700">{categories[0].title}</span>
+													<span className="mt-1 text-gray-600 text-sm">{new Date(_updatedAt).toDateString()}</span>
 												</div>
-												<div class="md:flex-grow">
-													<h2 class="text-2xl font-medium text-gray-900 title-font mb-2">{title}</h2>
-													<p class="leading-relaxed">{description[0].children[0].text}</p>
+												<div className="md:flex-grow">
+													<h2 className="text-2xl font-medium text-gray-900 title-font mb-2">{title}</h2>
+													<p className="leading-relaxed">{description[0].children[0].text}</p>
 													
 													<a className="text-indigo-500 inline-flex mt-4">
 														<Link href="/post/[slug]" as={`/post/${slug.current}`}>
 															Learn More
 														</Link>{' '}
-														<svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
+														<svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
 															<path d="M5 12h14"></path>
 															<path d="M12 5l7 7-7 7"></path>
 														</svg>
