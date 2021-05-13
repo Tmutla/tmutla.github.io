1daa950eaba7efd8a7460171e182c2bd16b9f8ed
Joel Corey <joelcorey@fastmail.com>
2021-05-13 12:23:05 -0700

    add back case studies

---
+++ pages/case-studies.js
@@ -0,0 +1,37 @@
+import Link from 'next/link'
+import Head from 'next/head'
+
+import Header from '/component/Header/Header'
+import HeroTitle from '/component/HeroTitle/HeroTitle'
+import Footer from '/component/Footer/Footer'
+
+export default function Page() {
+  return (
+    <div className="">
+      <Head>
+        <title>Tmutla</title>
+        <meta name="description" content="Tmutla Security" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="">
+			
+				<Header />
+
+				<HeroTitle
+					title="Case Studies"
+				/>
+				
+				<section className="text-gray-600 body-font">
+					<div className=" px-5 mx-auto flex flex-col">
+					
+					</div>
+				</section>
+			        
+      </main>
+
+			<Footer />
+
+    </div>
+  )
+}
