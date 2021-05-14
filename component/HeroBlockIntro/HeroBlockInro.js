79107a5445cd6266ac26817ec6143e43d1f2f8f1
Joel Corey <joelcorey@fastmail.com>
2021-05-14 09:36:53 -0700

    continue breakpoint adjustments to HeroBlockIntro component

--- component/HeroBlockIntro/HeroBlockInro.js
+++ component/HeroBlockIntro/HeroBlockInro.js
@@ -10,10 +10,11 @@ function HeroBlockIntro() {
 				{/* content */}
 				{/* <div className="mt-12 absolute w-full h-full flex flex-col place-content-center"> */}
 				<div className="absolute w-full h-full pt-4">
-					<p className="m-2 text-vw-8 md:text-vw-4 lg:text-vw-3 m-5 text-vw-4 text-black text-opacity-85">
-						Evolve beyond threats
+					<p className="m-2 text-vw-4 sm:text-vw-5  text-black text-opacity-85">
+						{/* Evolve beyond threats */}
+						Building The Foundations of Cybersecurity 
 					</p>
-					<div className="leading-7 md:leading-6 text-vw-4 md:text-vw-3 lg:text-vw-2 2xl:text-vw-1 max-w-xl lg:max-w-4xl xl:max-w-5xl border-t-4 border-solid border-red">
+					<div className="leading-8 md:leading-6 text-vw-3 md:text-vw-3 lg:text-vw-2 2xl:text-vw-1 max-w-xl md:max-w-3xl lg:max-w-4xl xl:max-w-5xl border-t-4 border-solid border-red">
 					
 						<p className="m-3 text-white-dark text-opacity-85">
 							As security threats increase in sophistication and complexity, trying to keep up can mean falling behind. Strengthening your security program requires foresight into when, where, and how attacks will occur.
