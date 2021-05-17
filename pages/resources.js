7a887c760df35be9eec8a614b518433ef2e2a051
Joel Corey <joelcorey@fastmail.com>
2021-05-17 10:07:39 -0700

    remove button

--- pages/resources.js
+++ pages/resources.js
@@ -165,7 +165,7 @@ export default function Resources() {
 							))}
 	
 						</div>
-						<button className="flex mx-auto mt-16 text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+						
 					</div>
 				</section>
         
