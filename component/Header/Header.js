7dd1d449ee457459c03aaf3f8e12974776de4b98
Joel Corey <joelcorey@fastmail.com>
2021-05-18 06:49:13 -0700

    remove fragment

--- component/Header/Header.js
+++ component/Header/Header.js
@@ -93,45 +93,43 @@ export default function Header() {
               <Popover.Group as="nav" className="hidden md:flex space-x-10">
                 <Popover className="relative">
                   {({ open }) => (
-                    <>
-                     
+                    
+										<Transition
+											show={open}
+											as={Fragment}
+											enter="transition ease-out duration-200"
+											enterFrom="opacity-0 translate-y-1"
+											enterTo="opacity-100 translate-y-0"
+											leave="transition ease-in duration-150"
+											leaveFrom="opacity-100 translate-y-0"
+											leaveTo="opacity-0 translate-y-1"
+										>
+											<Popover.Panel
+												static
+												className="absolute bottom-0 z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2"
+											>
+												<div className="shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
+													<div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
+														{menuLinks.map((item) => (
+															<Link
+																key={item.name}
+																href={item.href}
+																className="-m-3 p-3 flex items-start hover:bg-gray-50"
+															>
+																<a className="flex">
+																	<item.icon className="flex-shrink-0 h-6 w-6 text-red" aria-hidden="true" />
+																	<div className="ml-4">
+																		<p className="text-base font-medium text-gray-900">{item.name}</p>
+																		<p className="mt-1 text-sm text-gray-500">{item.description}</p>
+																	</div>
+																</a>
+															</Link>
+														))}
+													</div>
+												</div>
+											</Popover.Panel>
+										</Transition>
 
-                      <Transition
-                        show={open}
-                        as={Fragment}
-                        enter="transition ease-out duration-200"
-                        enterFrom="opacity-0 translate-y-1"
-                        enterTo="opacity-100 translate-y-0"
-                        leave="transition ease-in duration-150"
-                        leaveFrom="opacity-100 translate-y-0"
-                        leaveTo="opacity-0 translate-y-1"
-                      >
-                        <Popover.Panel
-                          static
-                          className="absolute bottom-0 z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2"
-                        >
-                          <div className="shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                            <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                              {menuLinks.map((item) => (
-                                <Link
-                                  key={item.name}
-                                  href={item.href}
-                                  className="-m-3 p-3 flex items-start hover:bg-gray-50"
-                                >
-																	<a className="flex">
-																		<item.icon className="flex-shrink-0 h-6 w-6 text-red" aria-hidden="true" />
-																		<div className="ml-4">
-																			<p className="text-base font-medium text-gray-900">{item.name}</p>
-																			<p className="mt-1 text-sm text-gray-500">{item.description}</p>
-																		</div>
-																	</a>
-                                </Link>
-                              ))}
-                            </div>
-                          </div>
-                        </Popover.Panel>
-                      </Transition>
-                    </>
                   )}
                 </Popover>
 
