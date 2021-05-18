177c7ef2462be34f5dd9d2059979a27a6bd3c631
Joel Corey <joelcorey@fastmail.com>
2021-05-18 08:00:21 -0700

    fix error

--- component/HeaderBreachReport/HeaderBreachReport.js
+++ component/HeaderBreachReport/HeaderBreachReport.js
@@ -4,7 +4,8 @@ export default function HeaderBreachReport() {
 
 	return (
 		<div class="hidden md:flex flex-row items-center justify-end m-5 sm:hiden md:hidden lg:flex">
-			<Link>
+			<Link href="/report-breach">
+				
 				<button class="transition duration-500 ease-in-out transform hover:-translate-x-1 hover:scale-110 p-3 bg-red text-black hover:bg-red-light">REPORT A BREACH</button>
 			</Link>
 			<a class="p-3 pl-8" href="tel:1-800-123-4567">1-800-123-4567</a>
