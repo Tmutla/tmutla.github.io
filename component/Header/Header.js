e1fc302a4630718a6b1b916bf940520744c74ff8
Joel Corey <joelcorey@fastmail.com>
2021-05-18 06:48:00 -0700

    adjust header bg color, margin spacing

--- component/Header/Header.js
+++ component/Header/Header.js
@@ -69,11 +69,11 @@ function classNames(...classes) {
 
 export default function Header() {
   return (
-    <Popover className="relative w-full bg-white z-30">
+    <Popover className="relative w-full z-30">
       {({ open }) => (
         <>
           <div className="w-full mx-auto sm:px-6">
-            <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
+            <div className="flex justify-between items-center border-b-2 border-gray-100 md:justify-start md:space-x-10">
               <div className="flex justify-start lg:w-0 lg:flex-1">
                 <a href="/">
                   <span className="sr-only">Workflow</span>
